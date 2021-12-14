e13d71344be006b5338dfa85bf99677355c6defb
okarpenqa <okarpenqa@gmail.com>
2021-12-14 11:08:17 +0200

    .

---
+++ lesson_002_py_internship/main.py
@@ -0,0 +1,23 @@
+def key_item(item):
+    return item[1]
+
+
+main_list = [[1, 3, 3, 4], [2, 1, 3, 5], [4, 0, 1, 7], [5, 2, 1, 0], [0, 4, 8, 3]]
+main_list.sort(key=key_item)
+print('1.', main_list)
+print('_____________________________________________________________________________________________')
+main_dictionary = {}
+for element in main_list:
+    main_dictionary[element[1]] = element[0:]
+    main_dictionary[element[1]].pop(1)
+print('2.', main_dictionary)
+print('_____________________________________________________________________________________________')
+for key, value in main_dictionary.items():
+    value.sort(reverse=True)
+print('3.', main_dictionary)
+print('_____________________________________________________________________________________________')
+list_set = set([item for sublist in main_dictionary.values() for item in sublist])
+print('4.', list_set)
+print('_____________________________________________________________________________________________')
+string = ', '.join(map(str, list_set))
+print('5.', string)
